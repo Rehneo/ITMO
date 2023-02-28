cf29770839eae2f0b28e2478a5b80ff333a239c4
Rehneo <126577998+Rehneo@users.noreply.github.com>
2023-02-28 21:06:23 +0300

    Delete src directory

--- src/Exceptions/InvalidDataException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-/**
- * Базовый класс для всех исключений, связанных с датой.
- */
-public class InvalidDataException extends Exception {
-    public InvalidDataException(String message){
-        super(message);
-    }
-}
--- src/Exceptions/InvalidDateFormatException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-/**
- * Исключение, выбрасываемое когда формат даты не соответствует заданному формату.
- */
-public class InvalidDateFormatException extends InvalidDataException {
-    public InvalidDateFormatException(){
-        super("Формат даты должен быть yyyy-MM-dd HH:mm");
-    }
-}
